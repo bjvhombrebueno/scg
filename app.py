d51367b1bd2704a614f320049954e46fd5978310
bjvhombrebueno <benjoseph.hombrebueno@gmail.com>
2024-05-31 13:50:11 +1200

    fixed format on booking page

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect
-from flask import url_for_flask
+from flask import url_for
 import re
 from datetime import datetime
 from datetime import date
